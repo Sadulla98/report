9c8080d7d04bac0c73a11fcec92bd44b7124bf91
Sadulla98 <57902210+Sadulla98@users.noreply.github.com>
2022-11-09 13:32:54 +0500

    Application model
    creating only 1 application a day is done

--- app/Http/Controllers/AplicationController.php
+++ app/Http/Controllers/AplicationController.php
@@ -6,6 +6,7 @@ use App\Jobs\SendEmailJob;
 use App\Mail\ApplicationCreated;
 use App\Models\Aplication;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
@@ -18,6 +19,10 @@ class AplicationController extends Controller
             'message' => 'required',
             'file' => 'file|mimes:jpg,png,pdf',
         ]);
+
+        if ($this->checkDate()){
+            return redirect()->back()->with('error', 'You can create only 1 application a day');
+        }
 
         if ($request->hasFile('file')){
             $name = $request->file('file')->getClientOriginalName();
@@ -39,5 +44,20 @@ class AplicationController extends Controller
         dispatch(new SendEmailJob($application));
 
         return redirect()->back();
+    }
+
+    protected function checkDate()
+    {
+        if (auth()->user()->applications()->latest()->first() == null){
+            return false;
+        }
+
+        $last_application = auth()->user()->applications()->latest()->first();
+        $last_app_date = Carbon::parse($last_application->created_at)->format('Y-m-d');
+        $today = Carbon::now()->format('Y-m-d');
+
+        if ($last_app_date == $today){
+            return true;
+        }
     }
 }
